09895a9b3eee51dbd0142df00aa0ab4f455d66a7
Rusty Tanaka <yuichi.tanaka@ordi.jp>
2021-07-27 22:08:45 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://rustytanaka:over2442ture@cluster0.xjsyk.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
